6e1af3e7cfa508dec28cd3343b23187224990316
李云昊 <liyunhao@joytrip.org>
2023-03-16 11:43:40 +0800

    add

---
+++ assets/datePicker-9cf26a63.js
@@ -0,0 +1,1 @@
+import{B as t,C as n}from"./app-ae1b9487.js";import{j as o,M as s}from"./framework-6b3c435d.js";class a extends t{constructor(r){super(r),this.renderType="datepicker"}renderComponent(){return o(s(n.DATA_PICKER),{...this.computedProps()},this.computedSlots())}}function c(e){return new a(e)}function m(e){return c(e).render()}export{c,m as r};
