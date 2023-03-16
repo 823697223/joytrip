12b0212810304ba4e3a18a4e3027bc42740af920
李云昊 <liyunhao@joytrip.org>
2023-03-16 17:32:49 +0800

    add

---
+++ assets/button-f386b4f5.js
@@ -0,0 +1,1 @@
+import{B as n,C as r}from"./app-7bb956f0.js";import{j as o,M as s}from"./framework-6b3c435d.js";class p extends n{constructor(e){super(e),this.renderType="button"}renderComponent(){return o(s(r.BUTTON),{...this.computedProps()},()=>this.props.content)}}function u(t){return new p(t)}function c(t){return u(t).render()}export{u as c,c as r};
---
+++ assets/text-56a2e689.js
@@ -0,0 +1,1 @@
+import{B as t}from"./app-7bb956f0.js";import{j as n}from"./framework-6b3c435d.js";class o extends t{constructor(r){super(r),this.renderType="text"}renderComponent(){return n("div",{...this.computedProps()},this.props.text)}}function s(e){return new o(e)}function i(e){return s(e).render()}export{s as c,i as r};
