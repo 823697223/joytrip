d502dbfdbaec191cd91dd461ed5abd9d9ac5b147
李云昊 <liyunhao@joytrip.org>
2023-04-26 11:10:23 +0800

    add

--- assets/text-c0b30b05.js
+++
@@ -1,1 +0,0 @@
-import{B as t}from"./app-741b3775.js";import{j as n}from"./framework-fce7c3d2.js";class o extends t{constructor(r){super(r),this.renderType="text"}renderComponent(){return n("div",{...this.computedProps(),data:void 0},this.props.text)}}function s(e){return new o(e)}function a(e){return s(e).render()}export{s as c,a as r};
